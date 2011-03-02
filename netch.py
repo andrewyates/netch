6368b4e970c1c7d93592fa6aae14aeac73283d2c
Andrew Yates <andrew.yates@gmail.com>
2011-03-02 00:55:32 -0600

    add gplv2 license

--- netch.py
+++ netch.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# This file is a part of netch, a tool for monitoring network connection status
+# Copyright (C) 2011 by Andrew Yates <andrew.yates@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import base64
 import errno
